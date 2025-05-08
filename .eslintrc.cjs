3cbea86a198471176979f516ee92aa32009d8d77
FlowOFF <nettoaeb1@gmail.com>
2025-05-08 01:23:57 -0300

    refactor: reorganiza estilos do Tailwind em arquivos separados

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,39 +1,18 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-  },
+  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '*.css'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
+  plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    'no-unused-vars': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        semi: true,
-        trailingComma: 'all',
-        printWidth: 100,
-        tabWidth: 2,
-      },
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
     ],
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
   },
 };
